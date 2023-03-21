8ad9f796ab78e7e5610fbc8103ef49595a5bc4b0
Isaac Lyne <IsaactimothyLyne@gmail.com>
2023-03-21 23:57:28 +1300

    bug fixes
    but actually bug making

---
+++ DialogOption.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class DialogOption
+{
+    public string OptionText; // The text displayed for this option
+    public string TargetNodeId; // The ID of the target DialogNode this option leads to
+    public DialogCondition Condition;
+    public string OnOptionSelected = "";
+    public List<DialogOption> Options;
+    public string OptionNodeId;
+
+
+
+    public DialogOption(string optionText, string targetNodeId, string optionNodeId)
+    {
+        OptionText = optionText;
+        TargetNodeId = targetNodeId;
+        OptionNodeId = optionNodeId;
+    }
+
+}
+
+[System.Serializable]
+public class DialogCondition
+{
+    public enum ConditionType
+    {
+        None,
+        Boolean,
+        Float
+    }
+
+    public ConditionType Type;
+    public string ComponentName;
+    public string VariableName;
+    public bool BooleanValue;
+    public float FloatValue;
+    public ComparisonOperator FloatComparison;
+
+    public enum ComparisonOperator
+    {
+        GreaterThanOrEqual,
+        LessThanOrEqual
+    }
+}
